6d2e6065c754a03815b5d35bc2e1f4f89327af01
noong99 <77168149+noong99@users.noreply.github.com>
2021-12-04 18:16:28 +0900

    Update seoulpopulation_EDA.R

--- seoulpopulation_EDA.R
+++ seoulpopulation_EDA.R
@@ -39,29 +39,3 @@ ggplot(data=pop_group, aes(x=name, y=sum_population)) + geom_col()
 # 막대그래프 인구 큰 순서대로 정렬 
 ggplot(data=pop_group, aes(x=reorder(name,-sum_population), y=sum_population)) + geom_col()
 
-## SeoulWifi데이터의 위도, 경도를 이용한 버블차트
-SeoulWifi_df<-read.csv("SeoulWifi.csv")
-View(SeoulWifi_df)
-# 자치구별 위도 경도
-gu_cen<-SeoulWifi_df%>%
-  group_by(id)%>%
-  mutate(위도 = 위도,
-           경도 = 경도) %>% 
-  summarise(n = n())
-View(gu_cen)
-
-radius <- sqrt(pop_group$sum_population) # 원을 나타낼 반지름값
-symbols(pop_group$U2, UScrime$y, # x와 y의 좌표 위치     
-        circles=radius, # 각각 써클의 반지름값         
-        inches=0.4, # 각각 써클의 크기 조절값         
-        fg="white", # 각각 써클의 테두리 색         
-        bg="lightgray", # 각각 써클의 바탕색         
-        lwd=1.5, # 각각 써클의 테두리선 두께         
-        xlab="unemployment 35-39 males", # x 축 제목 설정         
-        ylab="crime rate", # y 축 제목 설정 
-        main="UScrime Data") # 제목 설정 
-
-text(UScrime$U2, UScrime$y, # 문자로 출력할 x,y 위치      
-     1:nrow(UScrime), # 문자로 출력할 값 
-     cex=0.8, # 글자 크기
-     col="brown") # 글자 색
