e25653da14b84d53576eacfd4f33efdea3736373
noong99 <77168149+noong99@users.noreply.github.com>
2021-12-04 12:34:08 +0900

    Update SeoulWifi_EDA.R
    gu_year이랑 막대그래프 추가했어요

--- SeoulWifi_EDA.R
+++ SeoulWifi_EDA.R
@@ -12,11 +12,34 @@ View(SeoulWifi.df)
   group_by(id, 위도, 경도) %>%
   summarise(count=n())'
 
+## 년도 상관없이 자치구별 공공와이파이 설치현황
 gu_count<-SeoulWifi.df%>%
   group_by(id)%>%
   summarise(n=n())
 
+## 법정동코드에 해당하는 자치구 이름 추가
+gu_count <- gu_count %>% mutate(name= case_when(
+  id == 11680 ~"강남구",id == 11740 ~"강동구",id == 11305 ~"강북구",
+  id == 11500 ~"강서구",id == 11620 ~"관악구",id == 11215 ~"광진구",
+  id == 11530 ~"구로구",id == 11545 ~"금천구",id == 11350 ~"노원구",
+  id == 11320 ~"도봉구",id == 11230 ~"동대문구",
+  id == 11590 ~"동작구",id == 11440 ~"마포구",
+  id == 11410 ~"서대문구",id == 11650 ~"서초구",
+  id == 11200 ~"성동구",id ==11290 ~"성북구", id == 11710 ~"송파구",
+  id == 11470 ~"양천구",id == 11560 ~"영등포구",
+  id == 11170 ~"용산구",id == 11380 ~"은평구",id == 11110 ~"종로구",
+  id == 11140 ~"중구", id == 11260 ~"중랑구", id == 11320 ~"도봉구"
+))
+
+# 년도별 자치구별 공공와이파이 설치현황
+gu_year <- SeoulWifi.df %>% 
+  group_by(설치년도,자치구) %>% 
+  summarise(n=n())
+
 #SeoulWifi.df<-SeoulWifi.df %>% filter(설치년도>=2018)
+## 히스토그램
+ggplot(data= gu_year, aes(x = reorder(자치구,-n), y=n)) +geom_col()
+ggplot(data= gu_count, aes(x = reorder(name,-n), y=n)) +geom_col()
 
 ## 지도 시각화
 library(ggmap)
